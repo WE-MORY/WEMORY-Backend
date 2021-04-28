6240a13767c7b8c7ee1cd1535855878f4c156819
YulHee Kim <dbfgml980413@gmail.com>
2021-04-29 02:41:54 +0900

    개발:: DB 모델 작성(율희)

--- WeMory/diary/models.py
+++ WeMory/diary/models.py
@@ -1,16 +1,15 @@
-
 from django.db import models
-#from user.models import User, Account
+from user.models import User, Account
 
 
 class Diary(models.Model):
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now_add=True)
-#    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
-#    account = models.ForeignKey(Account, on_delete=models.CASCADE, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, blank=True)
     image = models.ImageField(blank=True, null=True, upload_to="Post/%Y/%m/")
     title = models.CharField(max_length=50)
-    goal = models.PositiveIntegerField(default=0, null=True,)
+    goal = models.PositiveIntegerField(default=0, null=True)
 
     
 class Post(models.Model):
@@ -19,5 +18,5 @@ class Post(models.Model):
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now_add=True)
     image = models.ImageField(blank=True, null=True, upload_to="Post/%Y/%m/")
-#    author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
     diary = models.ForeignKey(Diary, blank=False, null=False, on_delete=models.CASCADE)
