467443c0a17f97d9542c02d02b24064cbb5effc6
YulHee Kim <dbfgml980413@gmail.com>
2021-05-02 21:21:12 +0900

    개발:: posts filter 기능 추가 (율희)

--- WeMory/diary/views.py
+++ WeMory/diary/views.py
@@ -9,7 +9,8 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from django.http.response import JsonResponse
 from rest_framework.parsers import JSONParser 
-from rest_framework import status
+from rest_framework import status, viewsets
+from django_filters.rest_framework import DjangoFilterBackend, filters
 
 @api_view(['GET','POST'])
 @permission_classes([AllowAny])
@@ -63,3 +64,43 @@ def goal_list(request):
         goal_serializers = GoalSerializer(goals, many=True)
         return Response(goal_serializers.data)
 
+
+# class PostList(generics.ListAPIView):
+#     serializer_class = PostSerializer
+
+#     def get_queryset(self):
+#         """
+#         This view should return a list of all the purchases
+#         for the currently authenticated user.
+#         """
+#         user = self.request.user
+#         return Purchase.objects.filter(purchaser=user)
+
+# class MoneyList(generics.ListAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = ProductSerializer
+#     filter_backends = [DjangoFilterBackend]
+#     filterset_fields = ['category', 'in_stock']
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('id', 'created_at', 'title', 'received_money')
+    http_method_names = ['get']
+ #   filterset_class = FundingKeywordFilter
+ #   lookup_field = 'like_count'
+
+    def get_queryset(self):
+        orderbyList = ['created_at']
+        q = self.request.GET.get('q')
+        key = self.request.GET.get('key','')
+
+        if q :
+            if key:
+                return Post.objects.filter(diary=q, created_at=str(key)).order_by(*orderbyList)
+            else:
+                return Post.objects.filter(diary=q).order_by(*orderbyList)
+        else:
+            return Post.objects.all().order_by(*orderbyList)
